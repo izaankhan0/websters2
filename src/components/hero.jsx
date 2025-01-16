d274303ac2ecc88f1016fb97466aab23f5a4b351
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-16 22:58:38 +0500

    rb shooting

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -9,7 +9,7 @@ function hero(){
     <img src="../../public/assets/media/bg-scroll.png" className="absolute abs-2" alt="" />
     <Nav />
     <div className="main">
-        <img src="../../public/assets/media/logo.png" className="logo" alt="" />
+        <img src="/../../public/assets/media/logo.png" className="logo" alt="" />
         <h1>Building your Digital Presence</h1>
         <button>Get your Website Now</button>
     </div>
