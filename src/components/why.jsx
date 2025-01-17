3ad256af89a05191891ca6d94a7ce3da5a73ddc7
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-17 18:10:05 +0500

    added a scroller to why page

--- src/components/why.jsx
+++ src/components/why.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../styles/why.css'
+import Scroll from "../../public/assets/media/bg-scroll-black.png"
 import resumeImg from "../../public/assets/media/resume.png";
 import webImg from "../../public/assets/media/web.png";
 import marImg from "../../public/assets/media/mar.png";
@@ -7,7 +8,10 @@ import allSitesImg from "../../public/assets/media/all-sites.png";
 
 function why(){
     return <div className="why" id='why'>
-
+        <img src={Scroll} className="absolute2 abs-12" alt="" />
+        <img src={Scroll} className="absolute2 abs-22" alt="" />
+        <img src={Scroll} className="absolute3 abs-32" alt="" />
+        <img src={Scroll} className="absolute3 abs-42" alt="" />
         <h1>Why need a Website?</h1>
         <div className="slides">
             <div className="slide-1 slide">
