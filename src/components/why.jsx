d052816f7622e2799e5a959f38419f67d272dd07
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-17 16:47:41 +0500

    added why content

--- src/components/why.jsx
+++ src/components/why.jsx
@@ -4,20 +4,20 @@ import '../styles/why.css'
 function why(){
     return <div className="why">
 
-        <h1></h1>
+        <h1>Why need a Website?</h1>
         <div className="slides">
             <div className="slide-1 slide">
-                <img src="" alt="" />
-                <p></p>
-                <img src="" alt="" />
+                <img src="../../public/assets/media/resume.png" alt="" />
+                <p>Resumes are Cringe, Get a Portfolio Website instead!</p>
+                <img src="../../public/assets/media/web.png" alt="" />
             </div>
             <div className="slide-2 slide">
-                <p></p>
-                <img src="" alt="" />
+                <p>Have an Online Presence! State your Achievements and Hobbies in a COOL way!</p>
+                <img src="../../public/assets/media/mar.png" alt="" />
             </div>
             <div className="slide-3 slide">
-                <p></p>
-                <img src="" alt="" />
+                <p>STAND OUT to your University or Employer!</p>
+                <img src="../../public/assets/media/all-sites.png" alt="" />
             </div>   
         </div>
 
