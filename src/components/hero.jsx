57ef7b5d106025aaae5ef3c606abf974d85d8983
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-17 15:48:50 +0500

    added logo differently

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../styles/hero.css';
 import '../styles/fonts.css';
 import Nav from "./nav";
+import Logo from "../../public/assets/media/logo.png"
 
 function hero(){
     return <div className="hero" id="hero">
@@ -9,7 +10,7 @@ function hero(){
     <img src="../../public/assets/media/bg-scroll.png" className="absolute abs-2" alt="" />
     <Nav />
     <div className="main">
-        <img src="/../../public/assets/media/logo.png" className="logo" alt="" />
+        <img src= {Logo} className="logo" alt="" />
         <h1>Building your Digital Presence</h1>
         <button>Get your Website Now</button>
     </div>
