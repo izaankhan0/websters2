d5b5dccd39dfaf3c1b44b67c854d1d35dffa9ca1
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-17 17:12:46 +0500

    styled why page

--- src/components/why.jsx
+++ src/components/why.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import '../styles/why.css'
+import resumeImg from "../../public/assets/media/resume.png";
+import webImg from "../../public/assets/media/web.png";
+import marImg from "../../public/assets/media/mar.png";
+import allSitesImg from "../../public/assets/media/all-sites.png";
 
 function why(){
     return <div className="why">
@@ -7,17 +11,17 @@ function why(){
         <h1>Why need a Website?</h1>
         <div className="slides">
             <div className="slide-1 slide">
-                <img src="../../public/assets/media/resume.png" alt="" />
+                <img src={resumeImg} alt="" />
                 <p>Resumes are Cringe, Get a Portfolio Website instead!</p>
-                <img src="../../public/assets/media/web.png" alt="" />
+                <img src={webImg} alt="" />
             </div>
             <div className="slide-2 slide">
                 <p>Have an Online Presence! State your Achievements and Hobbies in a COOL way!</p>
-                <img src="../../public/assets/media/mar.png" alt="" />
+                <img src={marImg} alt="" />
             </div>
             <div className="slide-3 slide">
+                <img src={allSitesImg} alt="" />
                 <p>STAND OUT to your University or Employer!</p>
-                <img src="../../public/assets/media/all-sites.png" alt="" />
             </div>   
         </div>
 
