d72447ab3b13dc03f92f751c8d4a0db1caee2a76
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-18 20:02:26 +0500

    added multiple logos to clients section and made it dynamic

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -1,31 +1,46 @@
 import React, { useState } from "react";
 import "../styles/clients.css";
-import taqseemLogo from "../../public/assets/media/taqseem-logo.jpeg";
-import Scroll from "../../public/assets/media/bg-scroll.png"
+import sayebaanLogo from "../../public/assets/media/sayebaan-logo.jpeg";
+import expressLogo from "../../public/assets/media/express-logo.png";
+import azeemLogo from "../../public/assets/media/azeem-logo.png";
+import taqseemLogo from "../../public/assets/media/taqseem-logo.png";
+import Scroll from "../../public/assets/media/bg-scroll.png";
 
-function clients() {
+function Clients() {
   return (
     <div className="clients" id="clients">
-        <img src={Scroll} className="absolute abs-5" alt="" />
-        <img src={Scroll} className="absolute abs-6" alt="" />
+      <img src={Scroll} className="absolute abs-5" alt="" />
+      <img src={Scroll} className="absolute abs-6" alt="" />
       <h2>Our Clients</h2>
       <div className="scroller">
         <div className="intn_list">
           <div className="int_icons">
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
+            <a href="https://taqseemfoundation.com">
+              <img src={taqseemLogo} alt="" />
+            </a>
+            <a href="https://sayebaan.com">
+              <img src={sayebaanLogo} alt="" />
+            </a>
+            <a href="https://izaankhan0.github.io/tqw">
+              <img src={expressLogo} alt="" />
+            </a>
+            <a href="https://izaankhan0.github.io/azeem">
+              <img src={azeemLogo} alt="" />
+            </a>
           </div>
           <div className="int_icons">
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
-            <img src={taqseemLogo} alt="" />
+            <a href="https://taqseemfoundation.com">
+              <img src={taqseemLogo} alt="" />
+            </a>
+            <a href="https://sayebaan.com">
+              <img src={sayebaanLogo} alt="" />
+            </a>
+            <a href="https://izaankhan0.github.io/tqw">
+              <img src={expressLogo} alt="" />
+            </a>
+            <a href="https://izaankhan0.github.io/azeem">
+              <img src={azeemLogo} alt="" />
+            </a>
           </div>
         </div>
       </div>
@@ -33,4 +48,4 @@ function clients() {
   );
 }
 
-export default clients;
+export default Clients;
