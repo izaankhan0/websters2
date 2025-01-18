5245162abe217d17e3474855f1aa0479b129f5bc
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-18 17:51:20 +0500

    added moving bg to clients section

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "../styles/clients.css";
 import taqseemLogo from "../../public/assets/media/taqseem-logo.jpeg";
+import Scroll from "../../public/assets/media/bg-scroll.png"
 
 function clients() {
   return (
     <div className="clients" id="clients">
+        <img src={Scroll} className="absolute abs-5" alt="" />
+        <img src={Scroll} className="absolute abs-6" alt="" />
       <h2>Our Clients</h2>
       <div className="scroller">
         <div className="intn_list">
