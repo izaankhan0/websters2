8edb7013ee595fe7255823f8e6870d5372729c81
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-18 23:05:31 +0500

    styled forms page

--- src/components/forms.jsx
+++ src/components/forms.jsx
@@ -1,38 +1,45 @@
-import React, { useState } from 'react'
-import '../styles/forms.css'
-import Startup from "../../public/assets/media/startup.png"
-import Personal from "../../public/assets/media/personal.png"
-import Portfolio from "../../public/assets/media/portfolio.png"
-import Company from "../../public/assets/media/company.png"
-import Blog from "../../public/assets/media/blog.png"
+import React, { useState } from "react";
+import "../styles/forms.css";
+import Startup from "../../public/assets/media/startup.png";
+import Personal from "../../public/assets/media/personal.png";
+import Portfolio from "../../public/assets/media/portfolio.png";
+import Company from "../../public/assets/media/company.png";
+import Blog from "../../public/assets/media/blog.png";
+import Scroll from "../../public/assets/media/bg-scroll-black.png";
 
 
-function forms(){
-    return <div className="forms" id="forms">
-        <h1>Choose the Website you wou want!</h1>
-        <div className="forms">
-            <div className="form-box">
-                <img src={Startup} alt="" />
-                <p>Startup Website</p>
-            </div>
-            <div className="form-box">
-                <img src={Personal} alt="" />
-                <p>Personal Website</p>
-            </div>
-            <div className="form-box">
-                <img src={Portfolio} alt="" />
-                <p>Portfolio Website</p>
-            </div>
-            <div className="form-box">
-                <img src={Company} alt="" />
-                <p>Company/Brand Website</p>
-            </div>
-            <div className="form-box">
-                <img src={Blog} alt="" />
-                <p>Blog Website</p>
-            </div>
-        </div>
+function Forms() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  return (
+    <div className="forms" id="forms">
+        <img src={Scroll} className="absolute abs-7" alt="" />
+        <img src={Scroll} className="absolute abs-8" alt="" />
+              
+      <h1>Choose the Website you want!</h1>
+      <div className="form">
+        {[{ src: Startup, label: "Startup Website" },
+          { src: Personal, label: "Personal Website" },
+          { src: Portfolio, label: "Portfolio Website" },
+          { src: Company, label: "Company/ Brand Website" },
+          { src: Blog, label: "Blog Website" }]
+          .map((item, index) => (
+            <a href="" key={index}>
+              <div
+                className={`form-box ${
+                  hoveredIndex !== null && hoveredIndex !== index ? "blur" : ""
+                }`}
+                onMouseEnter={() => setHoveredIndex(index)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <img src={item.src} alt="" />
+                <p>{item.label}</p>
+              </div>
+            </a>
+          ))}
+      </div>
     </div>
+  );
 }
 
-export default forms;
+export default Forms;
