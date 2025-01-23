b1f241eb17311dc5ec8d7da40278f0ffef19694f
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-24 00:39:23 +0500

    removed mentions of about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import './styles/App.css'
 import Hero from "./components/hero"
 import Why from "./components/why"
 import Clients from "./components/clients"
-// import About from "./components/about"
 import Forms from "./components/forms"
 import Process from "./components/process"
 import Reviews from "./components/reviews"
 import Contact from "./components/contacts"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import Startup from "./pages/startup.jsx"
 import Personal from "./pages/personal.jsx"
 import Portfolio from "./pages/portfolio.jsx"
