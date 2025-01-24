05b7a710814b17bf90a73473878135d40ce39cfa
Izaankhan0 <izaankhan.x@gmail.com>
2025-01-24 22:55:59 +0500

    reversed bg scroller from forms

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -3,8 +3,6 @@ import '../styles/blog.css';
 import '../styles/fonts.css';
 import FormsPage from '../components/forms-page';
 import { useEffect } from "react";
-import Scroll from "../../public/assets/media/bg-scroll-black.png"
-
 
 
 function blog(){
@@ -14,12 +12,6 @@ useEffect(() => {
       }, []);
 
     return <div className="blog">
-        <img src={Scroll} className="absolute abs-1" alt="" />
-                <img src={Scroll} className="absolute abs-2" alt="" />
-                <img src={Scroll} className="absolute-2 abs-1" alt="" />
-                <img src={Scroll} className="absolute-2 abs-2" alt="" />
-                <img src={Scroll} className="absolute-3 abs-1" alt="" />
-                <img src={Scroll} className="absolute-3 abs-2" alt="" />
         <FormsPage name="Blog" usd="$55" pkr="PKR 15,000" />
         <div className="google-form">
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeyROOJ3urWXxo5GyqjMI0K15unbi6YwZItjZfvhxDTf_QOBg/viewform?embedded=true" width="640" height="1800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -3,8 +3,6 @@ import '../styles/personal.css';
 import '../styles/fonts.css';
 import FormsPage from '../components/forms-page';
 import { useEffect } from "react";
-import Scroll from "../../public/assets/media/bg-scroll-black.png"
-
 
 
 
@@ -15,12 +13,6 @@ useEffect(() => {
       }, []);
 
     return <div className="personal">
-        <img src={Scroll} className="absolute abs-1" alt="" />
-                <img src={Scroll} className="absolute abs-2" alt="" />
-                <img src={Scroll} className="absolute-2 abs-1" alt="" />
-                <img src={Scroll} className="absolute-2 abs-2" alt="" />
-                <img src={Scroll} className="absolute-3 abs-1" alt="" />
-                <img src={Scroll} className="absolute-3 abs-2" alt="" />
         <FormsPage name="Personal" usd="$55" pkr="PKR 15,000" />
         <div className="google-form">
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScX-oFMjLKQDHAkCRbbOoLnHZvZiwZ4GVfzYhSVjC2kbrRQhw/viewform?embedded=true" width="640" height="1800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
